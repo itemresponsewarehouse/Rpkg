8fc460b5f55a439de28463bee7fce96b0ee7fb16
Hansol Lee <hansol@stanford.edu>
2024-11-11 22:06:10 -0800

    finish skeleton code structure

---
+++ data-raw/DATASET.R
@@ -0,0 +1,8 @@
+# ## code to prepare `DATASET` dataset goes here
+#
+# # Prior data processing steps on `ds_name` ...
+#
+# # Save the data set
+# save(ds_name, file = "data/ds_name.rda")
+# # Or...
+# usethis::use_data(ds_name, overwrite = TRUE)
