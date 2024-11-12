4b6eb1ea3e8b2e39c914287a26bb2e8ce40ef321
Hansol Lee <hansol@stanford.edu>
2024-11-12 02:08:35 -0800

    add unit tests and error handling for fetch_data()

--- R/data_query.R
+++ R/data_query.R
@@ -22,6 +22,7 @@ fetch_table <- function(name) {
 #' Fetch Data from a Specified Table
 #'
 #' Retrieves a specified table's data from the datasource, converting it to a data frame.
+#' If the conversion to a data frame fails, it stops and returns an informative error message.
 #'
 #' This function initializes the connection to the datasource (if not already initialized),
 #' retrieves the specified table, and converts it to a data frame. The function requires
@@ -37,9 +38,19 @@ fetch_table <- function(name) {
 #' @export
 fetch_data <- function(name) {
   table <- fetch_table(name)
-  df <- table$to_data_frame()
+
+  # Try to convert the table to a data frame, catching errors if the conversion fails
+  df <- tryCatch(
+    table$to_data_frame(),
+    error = function(e) {
+      stop(paste("Unable to fetch the dataset", shQuote(name),
+                 "from the IRW database. Please check the dataset name."))
+    }
+  )
+
   return(df)
 }
+
 
 
 
--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/articles/special-files.html
 
 library(testthat)
-library(irw)
+library(irwpkg)
 
-test_check("irw")
+test_check("irwpkg")
