f8dbca9892a7342fd0655bbbf9bedc0d392e9286
Hansol Lee <hansol@stanford.edu>
2025-01-26 16:23:13 -0800

    add example irw data to package

---
+++ R/DATA.R
@@ -0,0 +1,20 @@
+#' Example Data from IRW Database
+#'
+#' A sample dataset sourced from the IRW database to illustrate package functionality
+#' and enable users to experiment without needing authentication.
+#'
+#' @format A data frame with 4557 rows and 4 variables:
+#' \describe{
+#'   \item{id}{Unique identifier for each subject.}
+#'   \item{item}{Item identifier, specifying the question or measurement.}
+#'   \item{resp}{Response variable, indicating the response provided.}
+#'   \item{rater}{Rater identifier, specifying the rater who provided the response.}
+#' }
+#'
+#' @details
+#' This dataset is part of the IRW database and serves as an example for using package functions. 
+#' It includes basic variables typically present in psychometric datasets such as subject identifiers,
+#' item identifiers, and response variables.
+#'
+#' @source IRW Data Warehouse
+"swmd_mokken"
